3a80e47b3dfd69c540de462bf06e4314181024c2
Aaron Vigil <aaronvigil96@gmail.com>
2023-03-11 19:17:22 -0300

    Realizando actividad 6

--- ejercicio_6/src/ejercicio_6/Ejercicio_6.java
+++ ejercicio_6/src/ejercicio_6/Ejercicio_6.java
@@ -1,21 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    Realizar un programa que pida dos números enteros positivos por teclado y muestre por pantalla el siguiente menú:
+    El usuario deberá elegir una opción y el programa deberá mostrar el resultado por pantalla y luego volver al menú.
+    El programa deberá ejecutarse hasta que se elija la opción 5. Tener en cuenta que, si el usuario selecciona la opción 5,
+    en vez de salir del programa directamente, se debe mostrar el siguiente mensaje de confirmación: 
+    ¿Está seguro que desea salir del programa (S/N)? Si el usuario selecciona el carácter ‘S’ se sale del programa,
+    caso contrario se vuelve a mostrar el menú.
+*/
 package ejercicio_6;
 
-/**
- *
- * @author Yo
- */
-public class Ejercicio_6 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Ejercicio_6 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresá el primer numero");
+        int numA = entrada.nextInt();
+        System.out.println("Ingresá el segundo numero");
+        int numB = entrada.nextInt();
+        if(numA < 0 || numB < 0){
+            System.out.println("Solamente se acepetan numeros positivos");
+        }
+        
     }
     
 }
